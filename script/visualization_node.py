3b6c04486745923f9f9a538834cbc412ff1dedfe
Alexander Koufos <exoticdft@gmail.com>
2020-10-06 13:42:49 -0700

    Issue #2 & #3: Updates the visualization nodes
    - Adds docstrings to the classes and functions of the visualization_classespy file.
    - Cleans up code by formatting to meet the PEP8 standard.
    - Adds some logging information to the main visualization node.

--- script/visualization_node.py
+++ script/visualization_node.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 import rospy
-from visualization_classes import DesiredWaypointsVisualization, RoadGeometryVisualization, RoadNetworkVisualization
-from visualization_classes import egoTrackVisualizer, oppTrackVisualizer, PredictWaypointsVisualization
+
+from visualization_classes import DesiredWaypointsVisualization
+from visualization_classes import egoTrackVisualizer
+from visualization_classes import oppTrackVisualizer
+from visualization_classes import PredictWaypointsVisualization
+from visualization_classes import RoadGeometryVisualization
+from visualization_classes import RoadNetworkVisualization
 
 if __name__ == "__main__":
     try:
         rospy.init_node("viz_pub", anonymous=True)
+        ns = rospy.get_namespace()
+        rospy.loginfo_once(
+            "Initializing visualization node for namespace '{}'".format(ns)
+        )
+
         waypointsviz = DesiredWaypointsVisualization()
         road_viz = RoadGeometryVisualization()
         road_topo_viz = RoadNetworkVisualization()
         ego_track_viz = egoTrackVisualizer()
         opp_track_viz = oppTrackVisualizer()
         pre_waypoints_viz = PredictWaypointsVisualization()
+
         rospy.spin()
 
     except rospy.ROSInterruptException:
