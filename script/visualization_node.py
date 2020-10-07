9ac4c47653f98c395885bb385eb47e3f6e99835e
Alexander Koufos <exoticdft@gmail.com>
2020-10-07 13:37:46 -0700

    Issue #5: Updates map topology visualization class
    - Updates the map topology related visualization code to handle the map currently loaded on the Carla server.
    - Updates the docstring for the class.
    - Removes unused imports.

--- script/visualization_node.py
+++ script/visualization_node.py
@@ -8,6 +8,8 @@ from visualization_classes import PredictWaypointsVisualization
 from visualization_classes import RoadGeometryVisualization
 from visualization_classes import RoadNetworkVisualization
 
+import carla
+
 if __name__ == "__main__":
     try:
         rospy.init_node("viz_pub", anonymous=True)
@@ -16,9 +18,15 @@ if __name__ == "__main__":
             "Initializing visualization node for namespace '{}'".format(ns)
         )
 
+        # Get Carla server information
+        host = rospy.get_param("/carla/host")
+        port = rospy.get_param("/carla/port")
+        carla_client = carla.Client(host, port)
+        carla_world = carla_client.get_world()
+
         waypointsviz = DesiredWaypointsVisualization()
         road_viz = RoadGeometryVisualization()
-        road_topo_viz = RoadNetworkVisualization()
+        road_topo_viz = RoadNetworkVisualization(carla_world)
         ego_track_viz = egoTrackVisualizer()
         opp_track_viz = oppTrackVisualizer()
         pre_waypoints_viz = PredictWaypointsVisualization()
